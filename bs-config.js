46cf7c39d08e272e20fc8a8a8800058c13a267f5
Matt Campbell <cascadiamatt@gmail.com>
2018-09-04 08:56:01 -0700

    browser sync config

---
+++ bs-config.js
@@ -0,0 +1,118 @@
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+module.exports = {
+  "ui": {
+    "port": 3001,
+    "weinre": {
+      "port": 80
+    }
+  },
+  "files": [
+    "drupal/web/themes/gary/**/*.{js,yml,theme,module,php,inc,twig}",
+    "drupal/web/modules/gary/**/*.{js,yml,theme,module,php,inc,twig}"
+  ],
+  "watchEvents": [
+    "change"
+  ],
+  "watchOptions": {
+    "ignoreInitial": true,
+    "usePolling": true
+  },
+  "server": false,
+  "proxy": "localhost:80",
+  "port": 3000,
+  "middleware": false,
+  "serveStatic": [
+    {
+      route: "/themes/gary",
+      dir: "drupal/web/themes/gary"
+    },
+    {
+      route: "/modules/gary",
+      dir: "drupal/web/modules/gary"
+    }
+  ],
+  "ghostMode": {
+    "clicks": true,
+    "scroll": true,
+    "forms": {
+      "submit": true,
+      "inputs": true,
+      "toggles": true
+    }
+  },
+  "logLevel": "info",
+  "logPrefix": "BS",
+  "logConnections": false,
+  "logFileChanges": true,
+  "logSnippet": true,
+  "rewriteRules": [],
+  "open": false,//"local",
+  "browser": "default",
+  "cors": false,
+  "xip": false,
+  "hostnameSuffix": false,
+  "reloadOnRestart": false,
+  "notify": true,
+  "scrollProportionally": true,
+  "scrollThrottle": 0,
+  "scrollRestoreTechnique": "window.name",
+  "scrollElements": [],
+  "scrollElementMapping": [],
+  "reloadDelay": 500,
+  "reloadDebounce": 500,
+  "reloadThrottle": 1000,
+  "plugins": [],
+  "injectChanges": true,
+  "startPath": null,
+  "minify": true,
+  "host": null,
+  "localOnly": false,
+  "codeSync": true,
+  "timestamps": true,
+  "clientEvents": [
+    "scroll",
+    "scroll:element",
+    "input:text",
+    "input:toggles",
+    "form:submit",
+    "form:reset",
+    "click"
+  ],
+  "socket": {
+    "socketIoOptions": {
+      "log": false
+    },
+    "socketIoClientConfig": {
+      "reconnectionAttempts": 50
+    },
+    "path": "/browser-sync/socket.io",
+    "clientPath": "/browser-sync",
+    "namespace": "/browser-sync",
+    "clients": {
+      "heartbeatTimeout": 5000
+    }
+  },
+  "tagNames": {
+    "less": "link",
+    "scss": "link",
+    "css": "link",
+    "jpg": "img",
+    "jpeg": "img",
+    "png": "img",
+    "svg": "img",
+    "gif": "img",
+    "js": "script"
+  }
+};
