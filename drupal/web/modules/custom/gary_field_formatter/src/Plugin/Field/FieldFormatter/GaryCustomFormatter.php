10d7a03532721d9453e40d562f50310c40a19f36
Matt Campbell <cascadiamatt@gmail.com>
2018-10-22 11:42:57 -0700

    working field collection table formatter template. Starting to build form fields for ajax submit

--- drupal/web/modules/custom/gary_field_formatter/src/Plugin/Field/FieldFormatter/GaryCustomFormatter.php
+++ drupal/web/modules/custom/gary_field_formatter/src/Plugin/Field/FieldFormatter/GaryCustomFormatter.php
@@ -2,42 +2,28 @@
 
 namespace Drupal\gary_field_formatter\Plugin\Field\FieldFormatter;
 
-use Drupal\Core\Field\FormatterBase;
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Entity\Entity\EntityViewDisplay;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Plugin implementation of the 'Random_default' formatter.
+ * Plugin implementation of the 'fc_table_formatter' formatter.
  *
  * @FieldFormatter(
- *   id = "Random_default",
- *   label = @Translation("Random text"),
+ *   id = "fc_table_formatter",
+ *   label = @Translation("Custom FC Table Formatter"),
  *   field_types = {
- *     "Random"
+ *     "field_collection"
  *   }
  * )
  */
 class GaryCustomFormatter extends FormatterBase {
 
-  /**
-   * The entity manager service
-   *
-   * @var \Drupal\Core\Entity\EntityManagerInterface
-   */
-  protected $entityManager;
 
-  /**
-   * Construct a MyFormatter object
-   *
-   * @param \Drupal\Core\Entity\EntityManagerInterface $entityManager
-   *   The entity manager service
-   */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, EntityManagerInterface $entityManager) {
-    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
-
-    $this->entityManager = $entityManager;
-  }
-  
   /**
    * {@inheritdoc}
    */
@@ -51,42 +37,187 @@ class GaryCustomFormatter extends FormatterBase {
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    $element = [];
+    $elements = [];
 
-    foreach ($items as $delta => $item) {
-      // Render each element as markup.
-      $element[$delta] = ['#markup' => $item->value];
+    // Return nothing if there are no items in this array.
+    if (count($items) <= 0) {
+      return $elements;
     }
 
-    return $element;
+    //get name of field collection (id)
+    $fc_name = $items->getName();
+
+    // Get the table headers using the first row of field values to be rendered.
+    $fields = $this->getFieldList($items->get(0));
+
+    $elements['#header'] = array_values($fields);
+
+    // Loop through each field value to be rendered and render it.
+    foreach ($items as $delta => $item) {
+      $elements['#rows'][$delta] = $this->viewValue($item, array_keys($fields));
+    }
+    $numItems = count($elements['#rows']);
+
+    //we only need one record to load the entity form
+    // $myshit = $this->getFCForm($items->get(0), $fc_name);
+
+    // ksm(array_intersect($fields,$myshit));
+
+    //if building a custom form
+    $form = \Drupal::formBuilder()->getForm('Drupal\gary_field_formatter\Form\InlineForm', $items->get(0), $fc_name);
+
+    $elements['#theme'] = 'fc_table_formatter';
+
+    // $elements['#header'] = $myshit;
+    // $elements['#rows'] = $myshit;
+
+    // $elements['#rows'][$numItems] = $myshit;
+    // ksm($elements['#rows'][1]);
+
+    //vars to template
+    $elements['#caption'] = '';
+    $elements['#colgroups'] = [];
+    $elements['#sticky'] = FALSE;
+    $elements['#footer'] = NULL;
+    $elements['#empty'] = '';
+    $elements['#no_striping'] = 0;
+    $elements['#header_columns'] = $numItems;
+    $elements['#inline_form'] = $form;
+
+    // Add classes.
+    $elements['#attributes']['class'] = [
+      $items->name,
+      'fc_table',
+    ];
+
+    // $elements['#elements'] = $elements + ['#type' => 'table'];
+
+    // Return the data in a table.
+    return $elements;
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public static function defaultSettings() {
-    return [
-      // Declare a setting named 'text_length', with
-      // a default value of 'short'
-      'text_length' => 'short',
-    ] + parent::defaultSettings();
-  }
+  // /**
+  //  * {@inheritdoc}
+  //  */
+  // public function viewElements(FieldItemListInterface $items, $langcode) {
+  //   $element = [];
+  //
+  //   if (count($items) <= 0) {
+  //     return $elements;
+  //   }
+  //
+  //   foreach ($items as $delta => $item) {
+  //     // ksm($item);
+  //
+  //     if ($item->target_id !== NULL) {
+  //       $fc = FieldCollectionItem::load($item->target_id);
+  //       // $to_render = \Drupal::EntityTypeManager()->getViewBuilder('field_collection_item')->view($item->getFieldCollectionItem());
+  //
+  //       // $fields = \Drupal::FieldableEntityInterface()->getFields();
+  //       $fcArray = [];
+  //       foreach ($fc as $fckey => $field){
+  //         $fcArray[$delta][$fckey] = $field->value;
+  //       }
+  //     }
+  //     $element[$delta] = [
+  //       '#theme' => 'fc_table_formatter',
+  //       '#url' => $fcArray
+  //     ];
+  //   }
+  //
+  //   return $element;
+  // }
+  //
+  // /**
+  //  * {@inheritdoc}
+  //  */
+  // public static function defaultSettings() {
+  //   return [
+  //     // Declare a setting named 'text_length', with
+  //     // a default value of 'short'
+  //     'fc_table_formatter' => 'short',
+  //   ] + parent::defaultSettings();
+  // }
 
 
   /**
    * {@inheritdoc}
    */
   public function settingsForm(array $form, FormStateInterface $form_state) {
-    $element['text_length'] = [
-      '#title' => $this->t('Text length'),
-      '#type' => 'select',
-      '#options' => [
-        'short' => $this->t('Short'),
-        'long' => $this->t('Long'),
-      ],
-      '#default_value' => $this->getSetting('text_length'),
+    $element['fc_table_formatter'] = [
+      '#title' => $this->t('Use Ajax Inputs'),
+      '#type' => 'checkbox',
+      '#return_value' => true,
+      '#default_value' => true,
     ];
 
     return $element;
+  }
+
+  /**
+   * Get a list of fields and their machine names, ordered by weight.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One group of field values inside a field collection instance.
+   *
+   * @return array
+   *   An associative array of field labels keyed by field machine names.
+   */
+  protected function getFieldList(FieldItemInterface $item) {
+    $field_list = [];
+    $fields_by_weight = [];
+
+    // Build a list of the fields in the field collection's "full" display mode,
+    // along with their weights.
+    $entity = $item->getFieldCollectionItem();
+    $displays = EntityViewDisplay::collectRenderDisplays([$entity], 'full');
+    $display = $displays[$entity->bundle()];
+    foreach ($display->getComponents() as $name => $options) {
+      $fields_by_weight[$options['weight']] = $name;
+    }
+
+    // Now sort those fields by weight.
+    ksort($fields_by_weight);
+
+    // Now loop through the (weight-ordered) list, and build the final output:
+    // field labels keyed by machine name.
+    foreach ($fields_by_weight as $weight => $machine_name) {
+      $field_list[$machine_name] = $entity->getFieldDefinition($machine_name)->getLabel();
+    }
+
+    return $field_list;
+  }
+
+  /**
+   * Generate a table row corresponding to a single field collection instance.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One group of field values inside a field collection instance.
+   * @param array $fields_to_output
+   *   An array of field machine names, used to determine both which fields to
+   *   put in a row, and what order to put them in.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item, $fields_to_output = []) {
+    $row = [];
+
+    // Get display data for the fields in the field collection.
+    $entity = $item->getFieldCollectionItem();
+    $displays = EntityViewDisplay::collectRenderDisplays([$entity], 'full');
+    $display_components = $displays[$entity->bundle()]->getComponents();
+
+    // For each field to output, look for that field inside the field collection
+    // instance, render it without a label (since those will be in the header
+    // row), and add it to the current row.
+    foreach ($fields_to_output as $machine_name) {
+      $display_settings = $display_components[$machine_name];
+      $row[] = ['data' => $item->getFieldCollectionItem()->get($machine_name)->view([
+        'label' => 'hidden',
+      ] + $display_settings)];
+    }
+
+    return $row;
   }
 }
