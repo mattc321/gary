7eb38926286d08ac8214bb5c7e28130c1ec2a137
Matt Campbell <cascadiamatt@gmail.com>
2018-12-07 08:25:40 -0800

    added ajax_comments and some gary_comments stuff

---
+++ drupal/web/modules/custom/gary_comments/src/Form/GaryCommentsForm.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\gary_comments\Form;
+
+use Drupal\comment\CommentForm;
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Routing\CurrentRouteMatch;
+use Drupal\Core\Routing\RouteMatch;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Provides ajax enhancements to core default Comment form.
+ * Add submit handler to custom message tagging
+ *
+ * @package Drupal\gary_comments
+ */
+class GaryCommentsForm extends CommentForm {
+
+
+}
