fd31b33d4c2e07b96a7b3be5e2939c3d92a422f6
mattc321 <mcampbell@ashlandfood.coop>
2018-12-08 08:32:41 -0800

    working comment tags

--- drupal/web/modules/custom/gary_comments/src/Controller/CommentTag.php
+++ drupal/web/modules/custom/gary_comments/src/Controller/CommentTag.php
@@ -8,16 +8,43 @@ use Drupal\node\Entity\Node;
 
 class CommentTag extends ControllerBase {
 
-  public function AddMessage(EntityInterface $entity, $message = NULL) {
-    // ksm($entity->getOwnerId());
-    // ksm($entity->get('cid')->getValue());
+  protected $tagged_users = [];
+
+  protected $error_string = NULL;
+
+  public function CheckForTag(EntityInterface $entity) {
+
+    $body =  $entity->get('comment_body')->value;
+
+    //is there atleast one tag
+    if (stripos($body, '@') !==FALSE) {
+      //if users are tagged create messages for them
+      if (!empty($this->GetTaggedUsers($body))) {
+        foreach ($this->tagged_users as $key => $user) {
+          $this->AddMessage($entity, $user);
+        }
+      }
+
+      //check for a tag error
+      if (!empty($this->error_string)) {
+        $messenger = \Drupal::messenger();
+        $messenger->addMessage($this->error_string, $messenger::TYPE_WARNING);
+      }
+      return true;
+    }
+
+    return false;
+  }
+
+
+  public function AddMessage(EntityInterface $entity, $user) {
 
     $title = $entity->subject->value;
     $body =  $entity->get('comment_body')->value;
     $comment_id =  $entity->get('cid')->value;
     $content_id = $entity->getCommentedEntity()->id();
-    $by =  $entity->getOwnerId();
-    $to = $this->GetTaggedUser($body);
+    $by =  $entity->getOwner();
+    $to = $user;
 
     $message = Node::create([
        'type' => 'messages',
@@ -28,30 +55,33 @@ class CommentTag extends ControllerBase {
        'field_tag_user_by'=>$by,
        'field_tag_user_to'=>$to,
      ]);
-
-// $node->save();
+     $message->save();
+     $this->SendNotification($entity, $to, $by);
   }
 
-  public function SendNotification(EntityInterface $entity, $to = NULL, $from = NULL) {
-    // ksm($to);
-    // ksm($from);
+  public function SendNotification(EntityInterface $entity, $to, $from) {
+    $host_entity_bundle = $entity->getCommentedEntity()->bundle();
+    ksm($host_entity_bundle);
   }
 
-  protected function GetTaggedUser(string $body) {
+  protected function GetTaggedUsers($body) {
 
     $str = explode(" ",$body);
+
+    $users = [];
 
     foreach($str as $k=>$word){
       if(substr($word,0,1)=="@"){
         $uname = trim(substr($word,1));
         $tuser = user_load_by_name($uname);
-
         if(!empty($tuser)) {
-          return $tuser;
+          $users[] = $tuser;
         } else {
-          return NULL;
+          $this->error_string = t("You tagged a user that I could not find!");
         }
       }
     }
+    $this->tagged_users = $users;
+    return $users;
   }
 }
