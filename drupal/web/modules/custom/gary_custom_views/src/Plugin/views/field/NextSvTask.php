42bb7a1d2ce6602807876dea8067b2c6f59fbfb5
Matt Campbell <cascadiamatt@gmail.com>
2018-12-26 13:41:37 -0800

    added next sv date and updated queries

--- drupal/web/modules/custom/gary_custom_views/src/Plugin/views/field/NextSvTask.php
+++ drupal/web/modules/custom/gary_custom_views/src/Plugin/views/field/NextSvTask.php
@@ -12,7 +12,7 @@ use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
 
 /**
- * Field handler for display next service task.
+ * Field handler for display next site visit task.
  *
  * @ingroup views_field_handlers
  *
@@ -62,15 +62,20 @@ class NextSvTask extends FieldPluginBase {
     //The tid for Open tasks
     $tid = key($term);
 
-    $sql = "SELECT fd.title FROM gary.node__field_tasks ft
-      join node__field_task_list ftl on ft.field_tasks_target_id = ftl.entity_id
-      join node__field_task_status fts on ftl.entity_id = fts.entity_id
-      join node_field_data fd on ftl.entity_id = fd.nid
-      where ft.entity_id = node_field_data.nid
-      and ftl.field_task_list_target_id = ".$site_visit_nid."
-      and field_task_status_target_id = ".$tid."
-      and ft.deleted = 0
-      limit 0,1";
+    //select the open site visit type task by min date
+
+    $sql = "SELECT fd.title task_title
+                FROM gary.node__field_tasks ft
+                join node__field_task_list ftl on ft.field_tasks_target_id = ftl.entity_id
+                join node__field_task_status fts on ftl.entity_id = fts.entity_id
+                join node_field_data fd on ftl.entity_id = fd.nid
+                join node__field_task_due_date dd on ftl.entity_id = dd.entity_id
+                where ft.entity_id = node_field_data.nid
+                and ftl.field_task_list_target_id = ".$site_visit_nid."
+                and field_task_status_target_id = ".$tid."
+                and ft.deleted = 0
+                order by dd.field_task_due_date_value asc
+                limit 0,1";
     $params = [];
     $this->field_alias = $this->query->addField(NULL, "(".$sql.")", 'field_next_sv', $params);
     $this->addAdditionalFields();
