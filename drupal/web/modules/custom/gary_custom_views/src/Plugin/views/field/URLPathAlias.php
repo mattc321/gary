cbd26a9c3159b3f2760ba99641014bb006da0743
mattc321 <cascadiamatt@gmail.com>
2019-03-07 08:01:07 -0800

    added views path alias field

---
+++ drupal/web/modules/custom/gary_custom_views/src/Plugin/views/field/URLPathAlias.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\gary_custom_views\Plugin\views\field\URLPathAlias
+ */
+
+namespace Drupal\gary_custom_views\Plugin\views\field;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ * Field handler for displaying path alias.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("field_url_path_alias")
+ */
+class URLPathAlias extends FieldPluginBase {
+
+
+  /**
+   * Define the available options
+   * @return array
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    return $options;
+  }
+
+  /**
+   * Provide the options form.
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+  	$form['field_info'] = [
+      '#type' => 'item',
+      '#title' => t('Display the URL Alias')
+      ];
+    parent::buildOptionsForm($form, $form_state);
+  }
+
+  /**
+   * @{inheritdoc}
+   */
+  public function query() {}
+
+
+  /**
+   * @{inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    if (!empty($this->view->relationship)) {
+      if ($id = $values->_relationship_entities['node']) {
+        $alias = \Drupal::service('path.alias_manager')
+          ->getAliasByPath('/node/'.$id->id());
+        return $alias;
+      } else {
+        \Drupal::messenger()
+          ->addMessage(t('Error with the path alias field.'), 'error');
+        return '';
+      }
+    } else {
+      $alias = \Drupal::service('path.alias_manager')
+        ->getAliasByPath('/node/'.$values->_entity->id());
+      return $alias;
+    }
+    return '';
+  }
+
+
+}
