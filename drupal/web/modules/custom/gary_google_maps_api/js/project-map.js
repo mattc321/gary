f7374b873b80d8fd4f8b2f60dc85b55132f12f0b
matt campbell <mcampbell@ashlandfood.coop>
2019-05-20 07:46:21 -0700

    added xmlwriter output to drupalSettings. Working on map build

--- drupal/web/modules/custom/gary_google_maps_api/js/project-map.js
+++ drupal/web/modules/custom/gary_google_maps_api/js/project-map.js
@@ -1,80 +1,88 @@
-// async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDmjGKO04i7Kxm_3W1MU-70oc2W5F_Bi24&callback=initMap";
+/**
+ * @file
+ * Get fieldnames and append to select
+ */
+ (function($, Drupal, drupalSettings) {
 
-var customLabel = {
-  restaurant: {
-    label: 'R'
-  },
-  bar: {
-    label: 'B'
-  }
-};
+       xmlstuff = drupalSettings.gary_google_maps_api.project_xml_list;
+       var customLabel = {
+         restaurant: {
+           label: 'R'
+         },
+         bar: {
+           label: 'B'
+         }
+       };
 
-jQuery( document ).ready(function() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDmjGKO04i7Kxm_3W1MU-70oc2W5F_Bi24&callback=initMap';
-  document.body.appendChild(script);
-});
+       $(document).ready(function() {
+         console.log(1);
+         var script = document.createElement('script');
+         script.type = 'text/javascript';
+         script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDmjGKO04i7Kxm_3W1MU-70oc2W5F_Bi24&callback=initMap';
+         document.body.appendChild(script);
+       });
 
-  function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: new google.maps.LatLng(-33.863276, 151.207977),
-    zoom: 12
-  });
-  var infoWindow = new google.maps.InfoWindow;
+         function initMap() {
+         var map = new google.maps.Map(document.getElementById('map'), {
+           center: new google.maps.LatLng(-33.863276, 151.207977),
+           zoom: 12
+         });
+         var infoWindow = new google.maps.InfoWindow;
 
-    // Change this depending on the name of your PHP or XML file
-    downloadUrl('https://storage.googleapis.com/mapsdevsite/json/mapmarkers2.xml', function(data) {
-      var xml = data.responseXML;
-      var markers = xml.documentElement.getElementsByTagName('marker');
-      Array.prototype.forEach.call(markers, function(markerElem) {
-        var id = markerElem.getAttribute('id');
-        var name = markerElem.getAttribute('name');
-        var address = markerElem.getAttribute('address');
-        var type = markerElem.getAttribute('type');
-        var point = new google.maps.LatLng(
-            parseFloat(markerElem.getAttribute('lat')),
-            parseFloat(markerElem.getAttribute('lng')));
+           // Change this depending on the name of your PHP or XML file
+           downloadUrl('https://storage.googleapis.com/mapsdevsite/json/mapmarkers2.xml', function(data) {
+             var xml = data.responseXML;
+             var markers = xml.documentElement.getElementsByTagName('marker');
+             Array.prototype.forEach.call(markers, function(markerElem) {
+               var id = markerElem.getAttribute('id');
+               var name = markerElem.getAttribute('name');
+               var address = markerElem.getAttribute('address');
+               var type = markerElem.getAttribute('type');
+               var point = new google.maps.LatLng(
+                   parseFloat(markerElem.getAttribute('lat')),
+                   parseFloat(markerElem.getAttribute('lng')));
 
-        var infowincontent = document.createElement('div');
-        var strong = document.createElement('strong');
-        strong.textContent = name
-        infowincontent.appendChild(strong);
-        infowincontent.appendChild(document.createElement('br'));
+               var infowincontent = document.createElement('div');
+               var strong = document.createElement('strong');
+               strong.textContent = name
+               infowincontent.appendChild(strong);
+               infowincontent.appendChild(document.createElement('br'));
 
-        var text = document.createElement('text');
-        text.textContent = address
-        infowincontent.appendChild(text);
-        var icon = customLabel[type] || {};
-        var marker = new google.maps.Marker({
-          map: map,
-          position: point,
-          label: icon.label
-        });
-        marker.addListener('click', function() {
-          infoWindow.setContent(infowincontent);
-          infoWindow.open(map, marker);
-        });
-      });
-    });
-  }
+               var text = document.createElement('text');
+               text.textContent = address
+               infowincontent.appendChild(text);
+               var icon = customLabel[type] || {};
+               var marker = new google.maps.Marker({
+                 map: map,
+                 position: point,
+                 label: icon.label
+               });
+               marker.addListener('click', function() {
+                 infoWindow.setContent(infowincontent);
+                 infoWindow.open(map, marker);
+               });
+             });
+           });
+         }
 
 
 
-function downloadUrl(url, callback) {
-  var request = window.ActiveXObject ?
-      new ActiveXObject('Microsoft.XMLHTTP') :
-      new XMLHttpRequest;
+       function downloadUrl(url, callback) {
+         var request = window.ActiveXObject ?
+             new ActiveXObject('Microsoft.XMLHTTP') :
+             new XMLHttpRequest;
 
-  request.onreadystatechange = function() {
-    if (request.readyState == 4) {
-      request.onreadystatechange = doNothing;
-      callback(request, request.status);
-    }
-  };
+         request.onreadystatechange = function() {
+           if (request.readyState == 4) {
+             request.onreadystatechange = doNothing;
+             callback(request, request.status);
+           }
+         };
 
-  request.open('GET', url, true);
-  request.send(null);
-}
+         request.open('GET', url, true);
+         request.send(null);
+       }
 
-function doNothing() {}
+       function doNothing() {}
+
+ })(jQuery, Drupal, drupalSettings);
--- drupal/web/modules/custom/gary_google_maps_api/src/Controller/ProjectsMap.php
+++ drupal/web/modules/custom/gary_google_maps_api/src/Controller/ProjectsMap.php
@@ -7,6 +7,7 @@
 namespace Drupal\gary_google_maps_api\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Response;
 // use Drupal\Core\Ajax;
 // use Drupal\Core\Ajax\InvokeCommand;
 // use Drupal\views\Views;
@@ -18,20 +19,13 @@ use Drupal\Core\Controller\ControllerBase;
 
 class ProjectsMap extends ControllerBase {
 
+
+  private $geocodes;
+
   public function buildMap() {
 
-    // $build = [
-    //   '#content' => '',
-    //   '#theme' => 'gary_google_maps_api',
-    //   '#attached' => [
-    //     'library' => [
-    //       'gary_google_maps_api/project_map'
-    //     ]
-    //   ]
-    // ];
-
     $build = [
-      '#type' => 'inline_template',
+      '#content' => '',
       '#theme' => 'gary_google_maps_api',
       '#attached' => [
         'library' => [
@@ -40,8 +34,71 @@ class ProjectsMap extends ControllerBase {
       ]
     ];
 
+    // $build = [
+    //   '#type' => 'inline_template',
+    //   '#theme' => 'gary_google_maps_api',
+    //   '#attached' => [
+    //     'library' => [
+    //       'gary_google_maps_api/project_map'
+    //     ]
+    //   ]
+    // ];
+    $build['#attached']['drupalSettings']['gary_google_maps_api']['project_xml_list'] = $this->getXmlOutput();
+    // ksm($this->getXmlOutput());
     return $build;
 
+  }
+
+  public function getAddressXml() {
+
+  }
+
+  private function getXmlOutput() {
+    // ksm($response);
+    $doc = new \DOMDocument();
+    $doc->encoding = 'utf-8';
+    $doc->xmlVersion = '1.0';
+    $doc->formatOutput = TRUE;
+    $xml_file_name = 'project-list.xml';
+    $node = $doc->createElement('markers');
+    $paranode = $doc->appendChild($node);
+    $geocodes = $this->queryGeocodes();
+
+    foreach ($geocodes as $key => $geocode_array) {
+      $marker_node = $doc->createElement('marker');
+      $newnode = $paranode->appendChild($marker_node);
+
+      $newnode->setAttribute("id", $key);
+      $newnode->setAttribute("name", $geocode_array->account_node_title);
+      $newnode->setAttribute("address", $geocode_array->title);
+      $newnode->setAttribute("lat", $geocode_array->field_geolocation_lat);
+      $newnode->setAttribute("lng", $geocode_array->field_geolocation_lng);
+      $newnode->setAttribute("type", "");
+    }
+
+    return $doc->saveXML();
+    // return new Response(0);
+  }
+
+  private function queryGeocodes() {
+    $query = \Drupal::database()->select('node_field_data', 'project_node');
+    $query->addField('project_node', 'nid');
+    $query->addField('project_node', 'title');
+    $query->addJoin('left','node__field_geolocation','g','project_node.nid=g.entity_id');
+    $query->addJoin('left','node__field_account_reference','a','project_node.nid=a.entity_id');
+    $query->addJoin('left','node_field_data','account_node','a.field_account_reference_target_id=account_node.nid');
+    $query->addField('account_node', 'nid');
+    $query->addField('account_node', 'title');
+    $query->addField('g', 'field_geolocation_lat');
+    $query->addField('g', 'field_geolocation_lng');
+    $query->addField('g', 'field_geolocation_lat_sin');
+    $query->addField('g', 'field_geolocation_lat_cos');
+    $query->addField('g', 'field_geolocation_lng_rad');
+    $query->condition('project_node.type', 'projects');
+    $query->condition('project_node.status', 1);
+    $query->condition('g.bundle', 'projects');
+    $results = $query->execute()->fetchAll();
+    return $results;
   }
 
 }
