f8c1bba9e8b498d5cb35a38542cb573a3774f329
mattc321 <cascadiamatt@gmail.com>
2019-05-22 08:15:08 -0700

    minor map changes and gitignore add services

--- drupal/web/modules/custom/gary_google_maps_api/js/project-map.js
+++ drupal/web/modules/custom/gary_google_maps_api/js/project-map.js
@@ -22,7 +22,9 @@
        document.body.appendChild(script);
 
 
+
          function initMap() {
+
            var map = new google.maps.Map(document.getElementById('map'), {
              center: new google.maps.LatLng(center.lat, center.lng),
              zoom: Number(center.zoom)
