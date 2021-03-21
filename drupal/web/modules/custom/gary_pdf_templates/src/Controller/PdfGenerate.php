f1d53123204b6f7f1ec75d1a9b2846164cf717a6
Matt Campbell <cascadiamatt@gmail.com>
2021-03-21 09:24:28 -0700

    load fonts. fix field template. add logo. working.

--- drupal/web/modules/custom/gary_pdf_templates/src/Controller/PdfGenerate.php
+++ drupal/web/modules/custom/gary_pdf_templates/src/Controller/PdfGenerate.php
@@ -6,6 +6,9 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Messenger\Messenger;
 use Drupal\Core\Render\Renderer;
+use Drupal\gary_custom\GaryFunctions;
+use Drupal\node\Entity\Node;
+use Drupal\paragraphs\Entity\Paragraph;
 use Mpdf\Mpdf;
 use Mpdf\Output\Destination;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -63,25 +66,55 @@ class PdfGenerate extends ControllerBase
         }
       }
 
+      /** @var Node $template_entity */
       $template_entity = $this->entityTypeManager->getStorage('node')->load($request->query->get('tid'));
+
+      /** @var Node $entity */
       $entity = $this->entityTypeManager->getStorage('node')->load($request->query->get('eid'));
 
       $build  = [
         '#theme' => 'pdf_template',
         '#template_entity' => $template_entity,
-        '#entity' => $entity
+        '#entity' => $entity,
+        '#additional_vars' => [
+          'primary_contact' => $this->getPrimaryContact($entity)
+        ],
       ];
 
       $build['#attached']['library'][] = 'gary_pdf_templates/template';
 
+      //css
       $path = drupal_get_path(
           'module',
           'gary_pdf_templates'
         ) . '/css/pdf-template.css';
       $build['#css'] = '/'.$path;
 
+      //font
+      $fontPath = drupal_get_path(
+          'module',
+          'gary_pdf_templates'
+        ) . '/fonts/';
+
       $output = $this->renderer->renderRoot($build);
-      $mpdf = new Mpdf(['tempDir' => 'sites/default/files/pdfs/']);
+
+      $config = [
+        'fontDir' => [
+          $fontPath
+        ],
+        'fontdata' => [
+            'montserrat' => [
+              'R' => 'Montserrat-Regular.ttf',
+              'I' => 'Montserrat-Medium.ttf',
+            ]
+          ],
+        'default_font' => 'montserrat',
+        'tempDir' => 'sites/default/files/pdfs/'
+      ];
+
+      $mpdf = new Mpdf($config);
+      $mpdf->setHtmlHeader($template_entity->field_page_header->value);
+      $mpdf->setHtmlFooter($template_entity->field_page_footer->value);
       $mpdf->WriteHTML($output);
       $mpdf->Output($request->query->get('file') . '.pdf', Destination::DOWNLOAD);
       return new Response();
@@ -90,4 +123,32 @@ class PdfGenerate extends ControllerBase
     }
 
   }
+
+  private function getPrimaryContact(Node $entity)
+  {
+    if (!$entity->hasField('field_project_contacts')) {
+      return '';
+    }
+
+    $helper = new GaryFunctions();
+    $first = null;
+    foreach ($entity->get('field_project_contacts') as $index => $paragraphItem) {
+
+      $contactPgId = $paragraphItem->getValue();
+      $contact = Paragraph::load(array_shift($contactPgId));
+      $first = $index === 0 ? $contact : $first;
+
+      if ($contact->field_primary->value == 1) {
+        return $helper::loadEntityRef($contact, 'field_contact_reference');
+      }
+    }
+
+    if ($first) {
+      return $helper::loadEntityRef($first, 'field_contact_reference');
+    }
+
+    return '';
+
+  }
+
 }
